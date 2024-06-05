b11e8193892eb02fd4d28cd9e1439dd0d3406374
Artur Strzelecki <146848435+0Artur1@users.noreply.github.com>
2024-06-05 16:36:59 +0200

    Update obj_to_opengl.hpp

--- obj_to_opengl.hpp
+++ obj_to_opengl.hpp
@@ -20,4 +20,10 @@ bool parse_from_obj(const char* path,
     std::vector < float >& out_normals,
     std::vector <int>& number_vertex);
 
+bool another_parse_from_obj(const char* path,
+    std::vector < float >& out_vertices,
+    std::vector < float >& out_uvs,
+    std::vector < float >& out_normals,
+    std::vector <int>& number_vertex);
+
 #endif
